2176cfd8fea3bcaa23b4d6334622220847887176
ruthubasappa27 <ruthubasappa27@gmail.com>
2026-06-01 15:22:36 +0100

    Update prepare_gwas_smr.R

--- 06_SMR_analysis/prepare_gwas_smr.R
+++ 06_SMR_analysis/prepare_gwas_smr.R
@@ -3,15 +3,19 @@
 # Author: Ruthushree P Basappa
 # Date: May 2026
 
+
 library(data.table)
+library(here)
 
-# Load QC'd GWAS file
-gwas_qc <- fread("gwas_qc.csv")
+here::i_am("06_SMR_analysis/prepare_gwas_smr.R")
 
-# Create chr:bp SNP ID to match FB_Brain_2 format
+dir.create(here("results", "smr"), showWarnings = FALSE, recursive = TRUE)
+dir.create(here("references", "g1000_eur_chrbp"), showWarnings = FALSE, recursive = TRUE)
+
+gwas_qc <- fread(here("data", "gwas_qc.csv"))
+
 gwas_qc[, SNP_chrbp := paste0(CHR, ":", POS)]
 
-# Create SMR format file
 gwas_smr_chrbp <- gwas_qc[, .(
   SNP = SNP_chrbp,
   A1 = EA,
@@ -23,12 +27,17 @@ gwas_smr_chrbp <- gwas_qc[, .(
   N = NEFF
 )]
 
-fwrite(gwas_smr_chrbp, "gwas_smr_chrbp.txt", sep="\t")
+fwrite(gwas_smr_chrbp, here("data", "gwas_smr_chrbp.txt"), sep = "\t")
 cat("GWAS SMR file created:", nrow(gwas_smr_chrbp), "SNPs\n")
 
-# Also convert LD reference BIM file to chr:bp format
-system("mkdir -p g1000_eur_chrbp")
-system("cp 'g1000_eur 2/g1000_eur.fam' g1000_eur_chrbp/g1000_eur.fam")
-system("cp 'g1000_eur 2/g1000_eur.bed' g1000_eur_chrbp/g1000_eur.bed")
-system("awk '{print $1\"\\t\"$1\":\"$4\"\\t\"$3\"\\t\"$4\"\\t\"$5\"\\t\"$6}' 'g1000_eur 2/g1000_eur.bim' > g1000_eur_chrbp/g1000_eur.bim")
+source_prefix <- here("references", "g1000_eur", "g1000_eur")
+target_prefix <- here("references", "g1000_eur_chrbp", "g1000_eur")
+
+system(sprintf("cp '%s.fam' '%s.fam'", source_prefix, target_prefix))
+system(sprintf("cp '%s.bed' '%s.bed'", source_prefix, target_prefix))
+system(sprintf(
+  "awk '{print $1\"\\t\"$1\":\"$4\"\\t\"$3\"\\t\"$4\"\\t\"$5\"\\t\"$6}' '%s.bim' > '%s.bim'",
+  source_prefix, target_prefix
+))
 cat("LD reference converted to chr:bp format\n")
+
